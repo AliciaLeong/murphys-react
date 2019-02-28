240208efb540e601c7533cb3d9426f173a7ad583
Alicia Leong <alicial8@hawaii.edu>
2019-02-28 09:48:12 -1000

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Icon, Menu, Dropdown, Image, Grid, Header} from 'semantic-ui-react';
+import { Container, Icon, Menu, Dropdown, Image, Grid, List} from 'semantic-ui-react';
 
 class TopMenu extends React.Component {
 
@@ -31,20 +31,20 @@ class TopMenu extends React.Component {
   }
 }
 
+
 class Middle extends React.Component {
   render() {
     return(
         <div className="murphys-background">
           <Container>
-            <Grid columns={2}>
+            <Grid centered columns={2} verticalAlign="middle" className="vertical">
               <Grid.Column>
-                <Image scr="http://murphyshawaii.com/media/2014/04/murphyshawaiilogo.png"/>
+                <Image src="http://murphyshawaii.com/media/2014/04/murphyshawaiilogo.png"/>
               </Grid.Column>
               <Grid.Column>
-                <Container textAlign="center">
-                  <Header> A traditional downtown saloon and eatery located in the Honolulu Financial District, just one block off the waterfront, Murphy’s has been a haven for mariners, businessmen and locals since 1891.
-                   </Header>
-                </Container>
+                <h3> A traditional downtown saloon and eatery located in the Honolulu Financial District,
+                  just one block off the waterfront, Murphy’s has been a haven for mariners, businessmen and locals since 1891.
+                </h3>
               </Grid.Column>
             </Grid>
           </Container>
@@ -57,7 +57,37 @@ class Bottom extends React.Component {
   render(){
     return(
         <div className="footer-background">
-
+          <Container>
+            <Grid centered columns={3}>
+              <Grid.Column>
+                Lunch
+                <hr/>
+                <List>
+                  <List.Item>
+                    Monday – Friday: 11:00am – 2:30pm
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                Bar
+                <hr/>
+                <List>
+                  <List.Item>
+                    Monday – Friday: 11:00am – 2:30pm
+                  </List.Item>
+                </List>
+              </Grid.Column>
+              <Grid.Column>
+                Dinner
+                <hr/>
+                <List>
+                  <List.Item>
+                    Monday – Friday: 11:00am – 2:30pm
+                  </List.Item>
+                </List>
+              </Grid.Column>
+            </Grid>
+          </Container>
         </div>
     );
   }
